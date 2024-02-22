1ba09d9b3fc66091ec351119f3c882f0e7b7a181
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-02-22 19:24:05 +0300

    init: jwt.strategy

---
+++ src/oauth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('JWT_SECRET') ?? 'SECRET',
+    });
+  }
+
+  async validate(payload: { sub: number }) {
+    return { id: payload.sub };
+  }
+}
