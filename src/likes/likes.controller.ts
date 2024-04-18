e35e64ea679ec390df8678fa34d0b8a1e1bc43cf
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-18 08:20:32 +0300

    fix: likes-controller

--- src/likes/likes.controller.ts
+++ src/likes/likes.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body, Param, Delete } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { CreateLikeDto } from './dto/create-like.dto';
-import { UpdateLikeDto } from './dto/update-like.dto';
 
 @Controller('likes')
 export class LikesController {
@@ -18,21 +9,6 @@ export class LikesController {
   @Post()
   create(@Body() createLikeDto: CreateLikeDto) {
     return this.likesService.create(createLikeDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.likesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.likesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLikeDto: UpdateLikeDto) {
-    return this.likesService.update(+id, updateLikeDto);
   }
 
   @Delete(':id')
