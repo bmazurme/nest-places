a52206d94d5405e1ab7b5647529ccb45fc20ab69
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-18 08:17:29 +0300

    fix: tags-service

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -2,26 +2,34 @@ import { Injectable } from '@nestjs/common';
 
 import { CreateTagDto } from './dto/create-tag.dto';
 import { UpdateTagDto } from './dto/update-tag.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from './entities/tag.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TagsService {
+  constructor(
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
+  ) {}
+
   create(createTagDto: CreateTagDto) {
-    return 'This action adds a new tag';
+    return this.tagRepository.save(createTagDto);
   }
 
   findAll() {
-    return `This action returns all tags`;
+    return this.tagRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} tag`;
+    return this.tagRepository.findOneBy({ id });
   }
 
   update(id: number, updateTagDto: UpdateTagDto) {
-    return `This action updates a #${id} tag`;
+    return this.tagRepository.update(+id, updateTagDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} tag`;
+    return this.tagRepository.remove({ id });
   }
 }
