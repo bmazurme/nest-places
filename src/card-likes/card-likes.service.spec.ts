f22c230ea9997e0d21618da3c72fc517627d5adb
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-18 08:19:50 +0300

    fix: card-likes-service spec

--- src/card-likes/card-likes.service.spec.ts
+++ src/card-likes/card-likes.service.spec.ts
@@ -1,18 +1,87 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CardLikesService } from './card-likes.service';
+import { CardLike } from './entities/card-like.entity';
+import { UpdateCardLikeDto } from './dto/update-card-like.dto';
+import { CreateCardLikeDto } from './dto/create-card-like.dto';
 
 describe('CardLikesService', () => {
   let service: CardLikesService;
 
+  const cardLikesServiceMock = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CardLikesService],
-    }).compile();
+    })
+      .overrideProvider(CardLikesService)
+      .useValue(cardLikesServiceMock)
+      .compile();
 
     service = module.get<CardLikesService>(CardLikesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('.findAll() should call CardLikesService.findAll', () => {
+    jest.spyOn(service, 'findAll');
+    service.findAll();
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('.create() should call CardLikesService.create', () => {
+    const createTagDto = { id: 0, cardId: 1, userId: 2 } as CreateCardLikeDto;
+    const cardLike = { id: 0, cardId: 1, userId: 2 } as CardLike;
+
+    jest.spyOn(cardLikesServiceMock, 'create').mockReturnValue(cardLike);
+
+    const result = service.create(createTagDto);
+
+    expect(result).toEqual(cardLike);
+    expect(service.create).toHaveBeenCalled();
+    expect(service.create).toHaveBeenCalledWith(cardLike);
+  });
+
+  it('.findOne() should call CardLikesService.findOne', () => {
+    const id = '0';
+    const cardLike = { id: 0, cardId: 1, userId: 2 } as CardLike;
+
+    jest.spyOn(cardLikesServiceMock, 'findOne').mockReturnValue(cardLike);
+
+    const result = service.findOne(+id);
+
+    expect(result).toEqual(cardLike);
+    expect(service.findOne).toHaveBeenCalled();
+    expect(service.findOne).toHaveBeenCalledWith(0);
+  });
+
+  it('.update() should call CardLikesService.update', () => {
+    const id = '1';
+    const updateCardDto = { id: 0, cardId: 1, userId: 2 } as UpdateCardLikeDto;
+    const cardLike = { id: 0, cardId: 1, userId: 2 } as CardLike;
+
+    jest.spyOn(cardLikesServiceMock, 'update').mockReturnValue(cardLike);
+
+    const result = service.update(+id, updateCardDto);
+
+    expect(result).toEqual(cardLike);
+    expect(service.update).toHaveBeenCalled();
+    expect(service.update).toHaveBeenCalledWith(+id, updateCardDto);
+  });
+
+  it('.remove() should call CardLikesService.remove', async () => {
+    const id = '1';
+    const cardLike = { id: 1, cardId: 1, userId: 2 } as CardLike;
+
+    jest.spyOn(cardLikesServiceMock, 'remove').mockReturnValue(cardLike);
+
+    const result = service.remove(+id);
+
+    expect(result).toEqual(cardLike);
+    expect(service.remove).toHaveBeenCalled();
+    expect(service.remove).toHaveBeenCalledWith(+id);
   });
 });
