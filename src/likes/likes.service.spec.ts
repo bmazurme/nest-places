1c9498326760896a212ff2e8c00b259ef02e2fe5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-18 08:18:09 +0300

    fix: likes-service spec

--- src/likes/likes.service.spec.ts
+++ src/likes/likes.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LikesService } from './likes.service';
+import { CreateLikeDto } from './dto/create-like.dto';
+import { Like } from './entities/like.entity';
 
 describe('LikesService', () => {
   let service: LikesService;
 
+  const likesServiceMock = {
+    create: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [LikesService],
-    }).compile();
+    })
+      .overrideProvider(LikesService)
+      .useValue(likesServiceMock)
+      .compile();
 
     service = module.get<LikesService>(LikesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('.create() should call LikesService.create', () => {
+    const createLikeDto = { name: 'Name' } as CreateLikeDto;
+    const like = { id: 0 } as Like;
+
+    jest.spyOn(likesServiceMock, 'create').mockReturnValue(like);
+
+    const result = service.create(createLikeDto);
+
+    expect(result).toEqual(like);
+    expect(service.create).toHaveBeenCalled();
+    expect(service.create).toHaveBeenCalledWith({ name: 'Name' });
+  });
+
+  it('.remove() should call LikesService.remove', async () => {
+    const id = '1';
+    const like = { id: 0 } as Like;
+
+    jest.spyOn(likesServiceMock, 'remove').mockReturnValue(like);
+
+    const result = service.remove(+id);
+
+    expect(result).toEqual(like);
+    expect(service.remove).toHaveBeenCalled();
+    expect(service.remove).toHaveBeenCalledWith(+id);
   });
 });
