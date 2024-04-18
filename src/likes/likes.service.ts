33eef006323e8306dcb5f94cce72ef57db5e8222
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-18 08:17:55 +0300

    fix: likes-service

--- src/likes/likes.service.ts
+++ src/likes/likes.service.ts
@@ -1,26 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
 import { CreateLikeDto } from './dto/create-like.dto';
-import { UpdateLikeDto } from './dto/update-like.dto';
+import { Like } from './entities/like.entity';
 
 @Injectable()
 export class LikesService {
+  constructor(
+    @InjectRepository(Like)
+    private readonly likeRepository: Repository<Like>,
+  ) {}
+
   create(createLikeDto: CreateLikeDto) {
-    return 'This action adds a new like';
-  }
-
-  findAll() {
-    return `This action returns all likes`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} like`;
-  }
-
-  update(id: number, updateLikeDto: UpdateLikeDto) {
-    return `This action updates a #${id} like`;
+    return this.likeRepository.save(createLikeDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} like`;
+    return this.likeRepository.save({ id });
   }
 }
