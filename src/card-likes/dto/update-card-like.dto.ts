b651be7e2fea9783e505cb86e7a73995eac4a4f5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-18 22:45:22 +0300

    fix: remove card-like module

--- src/card-likes/dto/update-card-like.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCardLikeDto } from './create-card-like.dto';
-
-export class UpdateCardLikeDto extends PartialType(CreateCardLikeDto) {}
