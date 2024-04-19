603672dd923dbb2375b381e6fe55ec8c44d798d2
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-19 20:13:00 +0300

    fix: create-user.dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,23 +1,6 @@
-import {
-  IsEmail,
-  // IsOptional,
-  // IsString,
-  // IsUrl,
-  // Length,
-  // ValidateIf,
-} from 'class-validator';
+import { IsEmail } from 'class-validator';
 
 export class CreateUserDto {
-  // @IsString()
-  // @IsOptional()
-  // @ValidateIf((dto) => dto.about !== '')
-  // @Length(2, 200)
-  // about: string;
-
-  // @IsUrl()
-  // @IsOptional()
-  // avatar: string;
-
   @IsEmail()
   email: string;
 }
