b8fa34f6392754384282a30db68a3695880cb6ce
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-19 18:49:44 +0300

    fix: create-card.dto

--- src/cards/dto/create-card.dto.ts
+++ src/cards/dto/create-card.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCardDto {}
+import {
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+  ValidateIf,
+} from 'class-validator';
+
+export class CreateCardDto {
+  @IsString()
+  @IsOptional()
+  @ValidateIf((dto) => dto.name !== '')
+  @Length(2, 200)
+  name: string;
+
+  @IsUrl()
+  @IsOptional()
+  link: string;
+}
