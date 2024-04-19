2e133909e40e76503d13ef7976d8c7acf449f48b
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-19 18:45:36 +0300

    fix: tags.module

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,10 +1,15 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 import { TagsService } from './tags.service';
 import { TagsController } from './tags.controller';
 
+import { Tag } from './entities/tag.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Tag])],
   controllers: [TagsController],
   providers: [TagsService],
+  exports: [TagsService],
 })
 export class TagsModule {}
