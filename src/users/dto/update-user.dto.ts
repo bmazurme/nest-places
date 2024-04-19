e5b4680f366783551b7933ccc10a69eb387a4aa8
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-19 17:13:22 +0300

    fix: update-user.dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+  ValidateIf,
+} from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @Length(2, 30)
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  @ValidateIf((dto) => dto.about !== '')
+  @Length(2, 200)
+  about: string;
+
+  @IsUrl()
+  @IsOptional()
+  avatar: string;
+}
