c88b85d0f88935d91833a4df6aa0da88e807fbe6
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-22 07:44:55 +0300

    fix: like.entity

--- src/likes/entities/like.entity.ts
+++ src/likes/entities/like.entity.ts
@@ -1,1 +1,15 @@
-export class Like {}
+import { Entity, ManyToOne } from 'typeorm';
+
+import { BaseEntity } from '../../base-entity';
+
+import { Card } from '../../cards/entities/card.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Like extends BaseEntity {
+  @ManyToOne(() => Card, (card) => card.like)
+  card: Card;
+
+  @ManyToOne(() => User, (user) => user.cards)
+  user: User;
+}
