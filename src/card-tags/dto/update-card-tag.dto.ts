0d6451d002c9efc30d1c3c8e9b8a705aa6f366b3
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-22 07:42:01 +0300

    fix: update-card-tag.dto

--- src/card-tags/dto/update-card-tag.dto.ts
+++ src/card-tags/dto/update-card-tag.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNumber } from 'class-validator';
+
 import { CreateCardTagDto } from './create-card-tag.dto';
 
-export class UpdateCardTagDto extends PartialType(CreateCardTagDto) {}
+export class UpdateCardTagDto extends PartialType(CreateCardTagDto) {
+  @IsNumber()
+  id: number;
+}
