5f1a80517259cc6c0b2148f26df5d785e9cf3387
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-22 07:43:06 +0300

    fix: card-tag.entity

--- src/card-tags/entities/card-tag.entity.ts
+++ src/card-tags/entities/card-tag.entity.ts
@@ -1,1 +1,21 @@
-export class CardTag {}
+import { Column, Entity, ManyToOne } from 'typeorm';
+
+import { BaseEntity } from '../../base-entity';
+
+import { Card } from '../../cards/entities/card.entity';
+import { Tag } from '../../tags/entities/tag.entity';
+
+@Entity()
+export class CardTag extends BaseEntity {
+  @Column()
+  public cardId: number;
+
+  @Column()
+  public tagId: number;
+
+  @ManyToOne(() => Card, (card) => card.id)
+  public card: Card;
+
+  @ManyToOne(() => Tag, (tag) => tag.id)
+  public tag: Tag;
+}
