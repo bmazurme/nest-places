36075ef1963b888a44a0412d5d07813b5c3b61ff
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-22 07:45:39 +0300

    fix: tag.entity

--- src/tags/entities/tag.entity.ts
+++ src/tags/entities/tag.entity.ts
@@ -1,1 +1,15 @@
-export class Tag {}
+import { Column, Entity, OneToMany } from 'typeorm';
+import { Length } from 'class-validator';
+
+import { BaseEntity } from '../../base-entity';
+import { CardTag } from '../../card-tags/entities/card-tag.entity';
+
+@Entity()
+export class Tag extends BaseEntity {
+  @Column()
+  @Length(2, 30)
+  name: string;
+
+  @OneToMany(() => CardTag, (cardTag) => cardTag.id)
+  cardTag: CardTag[];
+}
