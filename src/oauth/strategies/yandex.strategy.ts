154efdb2c5f28adb29720f068244b850012e33f5
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2024-04-24 08:12:33 +0300

    feat: yandex.strategy

---
+++ src/oauth/strategies/yandex.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy } from 'passport-yandex';
+
+@Injectable()
+export class YandexStrategy extends PassportStrategy(Strategy, 'yandex') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      clientID: configService.get('CLIENT_YANDEX_ID'),
+      clientSecret: configService.get('CLIENT_YANDEX_SECRET'),
+      callbackURL: 'http://localhost:3000/oauth/yandex/redirect',
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: (err: any, user: any, info?: any) => void,
+  ): Promise<any> {
+    // auth service - get user or create
+    // generate jwt
+    const { default_email } = profile._json;
+    const user = { email: default_email };
+    const payload = {
+      user,
+      accessToken,
+    };
+
+    done(null, payload);
+  }
+}
