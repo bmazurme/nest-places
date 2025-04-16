1497eb231d1deddbe6d109376b40bfb7a39d99aa
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-04-16 08:23:59 +0300

    fix: base-entity

--- src/base-entity.ts
+++ src/base-entity.ts
@@ -4,15 +4,21 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+// import { Expose, Exclude } from 'class-transformer';
+import { Exclude } from 'class-transformer';
+
+export const GROUP_USER = 'group_user_details';
 
 @Entity()
 export class BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Exclude()
   @CreateDateColumn()
   createdAt: Date;
 
+  @Exclude()
   @UpdateDateColumn()
   updatedAt: Date;
 }
