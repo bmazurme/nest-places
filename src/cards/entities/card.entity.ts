375393297491bb61f4b15cee59a2c4adfad189c9
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-04-18 22:22:07 +0300

    fix: card.entity

--- src/cards/entities/card.entity.ts
+++ src/cards/entities/card.entity.ts
@@ -1,11 +1,14 @@
-import { Column, Entity, JoinTable, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 import { IsUrl, Length } from 'class-validator';
+// import { Expose } from 'class-transformer';
 
 import { BaseEntity } from '../../base-entity';
 
 import { User } from '../../users/entities/user.entity';
 import { Like } from '../../likes/entities/like.entity';
 import { CardTag } from '../../card-tags/entities/card-tag.entity';
+
+// import { GROUP_USER } from '../../base-entity';
 
 @Entity()
 export class Card extends BaseEntity {
@@ -17,13 +20,14 @@ export class Card extends BaseEntity {
   @IsUrl()
   link: string;
 
+  // @Expose({ groups: [GROUP_USER] })
   @ManyToOne(() => User, (user) => user.cards)
   user: User;
 
   @OneToMany(() => Like, (like) => like.card)
   like: Like[];
 
-  @OneToMany(() => CardTag, (cardTag) => cardTag.id)
-  @JoinTable()
+  @OneToMany(() => CardTag, (cardTag) => cardTag.card)
+  // @JoinTable()
   cardTag: CardTag[];
 }
