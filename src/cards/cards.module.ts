60c4636ea7ff15804dc7e55fc73dfc9d6e9fb08c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-04-29 22:57:43 +0300

    fix: cards.module

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CardsService } from './cards.service';
+
 import { CardsController } from './cards.controller';
 import { Card } from './entities/card.entity';
+import { LikesModule } from '../likes/likes.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Card])],
+  imports: [TypeOrmModule.forFeature([Card]), LikesModule],
   controllers: [CardsController],
   providers: [CardsService],
   exports: [CardsService],
