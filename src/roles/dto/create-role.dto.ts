57e173186e30d15e8e303b09d7efa528b5f619d2
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-05-05 20:55:35 +0300

    fix: create-role.dto

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,9 +1,9 @@
-import { IsOptional, IsString, Length, ValidateIf } from 'class-validator';
+import { IsOptional, IsString, Length } from 'class-validator';
 
 export class CreateRoleDto {
   @IsString()
   @IsOptional()
-  @ValidateIf((dto) => dto.name !== '')
+  // @ValidateIf((dto) => dto.name !== '')
   @Length(2, 200)
   name: string;
 }
