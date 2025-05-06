c956a2025d056da91a40d0291de3e5b3446f65ad
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-05-06 13:45:26 +0300

    fix: user-roles.controller

--- src/user-roles/user-roles.controller.ts
+++ src/user-roles/user-roles.controller.ts
@@ -6,33 +6,44 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 
 import { UserRolesService } from './user-roles.service';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { Roles } from '../common/decorators/roles.decorator';
+import { Role } from '../common/decorators/role.enum';
+
+import { JwtGuard } from '../oauth/jwt.guard';
 
 import { CreateUserRoleDto } from './dto/create-user-role.dto';
 import { UpdateUserRoleDto } from './dto/update-user-role.dto';
 
+@UseGuards(JwtGuard, RolesGuard)
 @Controller('user-roles')
 export class UserRolesController {
   constructor(private readonly userRolesService: UserRolesService) {}
 
   @Post()
+  @Roles([Role.Admin])
   create(@Body() createUserRoleDto: CreateUserRoleDto) {
     return this.userRolesService.create(createUserRoleDto);
   }
 
   @Get()
+  @Roles([Role.Admin])
   findAll() {
     return this.userRolesService.findAll();
   }
 
   @Get(':id')
+  @Roles([Role.Admin])
   findOne(@Param('id') id: string) {
     return this.userRolesService.findOne(+id);
   }
 
   @Patch(':id')
+  @Roles([Role.Admin])
   update(
     @Param('id') id: string,
     @Body() updateUserRoleDto: UpdateUserRoleDto,
@@ -41,6 +52,7 @@ export class UserRolesController {
   }
 
   @Delete(':id')
+  @Roles([Role.Admin])
   remove(@Param('id') id: string) {
     return this.userRolesService.remove(+id);
   }
