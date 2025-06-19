82736b2e49a2b8222c64cc93fe4f4d46335d9797
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-06-19 21:44:27 +0300

    fix: update-user.dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -6,19 +6,23 @@ import {
   Length,
   ValidateIf,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty()
   @IsString()
   @Length(2, 30)
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   @ValidateIf((dto) => dto.about !== '')
   @Length(2, 200)
   about: string;
 
+  @ApiProperty()
   @IsUrl()
   @IsOptional()
   avatar: string;
