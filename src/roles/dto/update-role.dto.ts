70e0f3de4e56a690377c6e33e145300046812256
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-01 20:52:35 +0300

    fix: role dto doc

--- src/roles/dto/update-role.dto.ts
+++ src/roles/dto/update-role.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
+
 import { CreateRoleDto } from './create-role.dto';
 
+/**
+ * Data Transfer Object for updating existing roles.
+ * This class extends PartialType of CreateRoleDto, making all its properties optional.
+ * Allows partial updates of role fields, where only provided fields will be updated.
+ * Useful for scenarios where only specific role details need to be modified.
+ *
+ * Example usage:
+ * - Updating role name
+ * - Modifying role permissions
+ * - Changing role description
+ */
 export class UpdateRoleDto extends PartialType(CreateRoleDto) {}
