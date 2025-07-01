faf81f155c17452d4d2d38651fa482389627fd92
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-01 22:09:20 +0300

    fix: role dto doc

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,6 +1,20 @@
 import { IsOptional, IsString, Length } from 'class-validator';
 
+/**
+ * Data Transfer Object for creating a new role.
+ * This class represents the structure of data required to create a new role entity.
+ * All fields are optional but must meet specific validation criteria if provided.
+ */
 export class CreateRoleDto {
+  /**
+   * Role name
+   * @type {string}
+   * @description Name of the role
+   * @validation Must be a string
+   * @validation Length between 2 and 200 characters
+   * @example 'admin'
+   * @isRequired Optional
+   */
   @IsString()
   @IsOptional()
   // @ValidateIf((dto) => dto.name !== '')
