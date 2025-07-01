59319bec3a3ad05d67ad344a67f22510591c5b67
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-01 20:51:53 +0300

    fix: file dto doc

--- src/files/dto/create-file.dto.ts
+++ src/files/dto/create-file.dto.ts
@@ -1,1 +1,6 @@
+/**
+ * Data Transfer Object for creating a new file.
+ * This class represents the structure of data required to create a new file entity.
+ * Currently empty, but can be extended with specific file properties.
+ */
 export class CreateFileDto {}
--- src/files/dto/update-file.dto.ts
+++ src/files/dto/update-file.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/swagger';
+
 import { CreateFileDto } from './create-file.dto';
 
+/**
+ * Data Transfer Object for updating existing files.
+ * This class extends PartialType of CreateFileDto, making all its properties optional.
+ * Allows partial updates of file fields, where only provided fields will be updated.
+ * Useful for scenarios where only specific file details need to be modified.
+ */
 export class UpdateFileDto extends PartialType(CreateFileDto) {}
