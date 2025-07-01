631b128b5afe74e04d77c15069245d3fb1fc6988
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-01 20:53:12 +0300

    fix: tag dto doc

--- src/tags/dto/create-tag.dto.ts
+++ src/tags/dto/create-tag.dto.ts
@@ -1,6 +1,24 @@
 import { IsString, Length } from 'class-validator';
 
+/**
+ * Data Transfer Object for creating new tags.
+ * This class represents the structure of data required to create a new tag entity.
+ * Contains a single mandatory field for the tag name.
+ */
 export class CreateTagDto {
+  /**
+   * Tag name
+   * @type {string}
+   * @description Name of the tag
+   * @example 'important' or 'urgent'
+   * @validation Must be a string between 2 and 200 characters
+   * @isRequired
+   * @swagger
+   * type: string
+   * minLength: 2
+   * maxLength: 200
+   * example: 'important'
+   */
   @IsString()
   @Length(2, 200)
   name: string;
--- src/tags/dto/update-tag.dto.ts
+++ src/tags/dto/update-tag.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+
 import { CreateTagDto } from './create-tag.dto';
 
+/**
+ * Data Transfer Object for updating existing tags.
+ * This class extends PartialType of CreateTagDto, making all its properties optional.
+ * Allows partial updates of tag fields, where only provided fields will be modified.
+ * Useful for scenarios where only specific tag details need to be changed.
+ */
 export class UpdateTagDto extends PartialType(CreateTagDto) {}
