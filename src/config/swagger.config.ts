7f9b93a7d465f9231c4064b2f4fad8fa1c1f890c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-08 18:46:56 +0300

    fix: swagger.config doc

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,5 +1,10 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
+/**
+ * Configuration for Swagger documentation.
+ * This configuration sets up the basic structure and metadata for the Swagger UI.
+ * Includes title, description, version, and tags for organizing API endpoints.
+ */
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('NTLSTL place')
   .setDescription('The place API description')
