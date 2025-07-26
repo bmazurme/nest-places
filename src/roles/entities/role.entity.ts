fa681e112caa2c0c18d99d7d14a68386fc5a836d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-26 22:16:21 +0300

    fix: role.entity

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,8 +1,7 @@
-import { Column, Entity, JoinTable, ManyToMany, OneToMany } from 'typeorm';
+import { Column, Entity, JoinTable, ManyToMany } from 'typeorm';
 import { Length } from 'class-validator';
 
 import { BaseEntity } from '../../base-entity';
-import { UserRole } from '../../user-roles/entities/user-role.entity';
 import { User } from '../../users/entities/user.entity';
 
 /**
@@ -17,13 +16,11 @@ export class Role extends BaseEntity {
   @Length(2, 30)
   name: string;
 
-  @ManyToMany(() => User, (user) => user.userRoles)
+  @ManyToMany(() => User, (user) => user.roles)
+  @JoinTable({
+    name: 'userRoles',
+    joinColumn: { name: 'roleId', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'userId', referencedColumnName: 'id' },
+  })
   users: User[];
-
-  /**
-   * Link userRoles
-   */
-  @OneToMany(() => UserRole, (userRole) => userRole.role)
-  @JoinTable()
-  userRoles: UserRole[];
 }
