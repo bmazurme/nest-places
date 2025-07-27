77bf7fef33785983512d0d35b5458a410a32e940
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-27 22:36:22 +0300

    fix: type-orm.config

--- src/config/type-orm.config.ts
+++ src/config/type-orm.config.ts
@@ -5,9 +5,7 @@ import { Tag } from '../tags/entities/tag.entity';
 import { Role } from '../roles/entities/role.entity';
 import { Like } from '../likes/entities/like.entity';
 import { Card } from '../cards/entities/card.entity';
-import { CardTag } from '../card-tags/entities/card-tag.entity';
 import { User } from '../users/entities/user.entity';
-import { UserRole } from '../user-roles/entities/user-role.entity';
 
 export const TypeOrmModuleConfig = TypeOrmModule.forRootAsync({
   imports: [ConfigModule],
@@ -18,7 +16,7 @@ export const TypeOrmModuleConfig = TypeOrmModule.forRootAsync({
     username: configService.get('POSTGRES_USER') ?? 'postgres',
     password: configService.get('POSTGRES_PASSWORD') ?? 'newPassword',
     database: configService.get('POSTGRES_DB') ?? 'nestplaces',
-    entities: [User, UserRole, Tag, Role, Like, Card, CardTag],
+    entities: [User, Tag, Role, Like, Card],
     synchronize: true,
   }),
   inject: [ConfigService],
