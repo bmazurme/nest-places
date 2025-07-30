8d2f7fc3049b9f54beadabc922a1434720770295
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-07-30 23:34:31 +0300

    fix: app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,11 +12,9 @@ import { TagsModule } from './tags/tags.module';
 import { CardsModule } from './cards/cards.module';
 import { LikesModule } from './likes/likes.module';
 import { RolesModule } from './roles/roles.module';
+import { FilesModule } from './files/files.module';
 
 import { TypeOrmModuleConfig } from './config';
-import { FilesModule } from './files/files.module';
-import { join } from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
