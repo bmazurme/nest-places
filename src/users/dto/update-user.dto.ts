e00a186a406bc18b8b2190858ce639d31ebd232b
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-01 23:48:45 +0300

    fix: update-user.dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,74 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
-import {
-  IsOptional,
-  IsString,
-  IsUrl,
-  Length,
-  ValidateIf,
-} from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
 import { CreateUserDto } from './create-user.dto';
+import { Role } from '../../roles/entities/role.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 /**
- * Data Transfer Object for updating user information.
- * This class extends PartialType of CreateUserDto, making all its properties optional.
- * Adds additional fields for updating user's name, about section, and avatar.
- * Allows partial updates where only provided fields will be modified.
+ * DTO для обновления пользователя
+ * Наследует поля от CreateUserDto, делая их опциональными
+ * Содержит обязательные поля для идентификации пользователя при обновлении
  */
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   /**
-   * User's name
-   * @type {string}
-   * @description User's display name
-   * @example 'John Doe'
-   * @validation Must be a string between 2 and 30 characters
-   * @isRequired
-   * @swagger
-   * type: string
-   * minLength: 2
-   * maxLength: 30
-   * example: 'John Doe'
+   * Роль пользователя в системе
+   *
+   * @example Role.ADMIN
+   * @description Новая роль пользователя, которую можно обновить
+   * @type {Role}
    */
-  @ApiProperty()
-  @IsString()
-  @Length(2, 30)
-  name: string;
-
-  /**
-   * User's about section
-   * @type {string}
-   * @description Optional description about the user
-   * @example 'Software developer with 5 years of experience'
-   * @isOptional
-   * @validation Valid if not empty and length between 2-200 characters
-   * @swagger
-   * type: string
-   * minLength: 2
-   * maxLength: 200
-   * example: 'Software developer with 5 years of experience'
-   */
-  @ApiProperty()
-  @IsString()
-  @IsOptional()
-  @ValidateIf((dto) => dto.about !== '')
-  @Length(2, 200)
-  about: string;
-
-  /**
-   * User's avatar URL
-   * @type {string}
-   * @description URL of the user's profile picture
-   * @example 'https://example.com/avatar.jpg'
-   * @isOptional
-   * @validation Must be a valid URL
-   * @swagger
-   * type: string
-   * format: uri
-   * example: 'https://example.com/avatar.jpg'
-   */
-  @ApiProperty()
-  @IsUrl()
-  @IsOptional()
-  avatar: string;
+  @ApiProperty({
+    description: 'Роль пользователя в системе',
+  })
+  role: Role;
 }
