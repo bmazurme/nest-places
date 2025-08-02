e2f347ba54c912f9cf3c234b9ceee85ad1443914
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-03 00:54:51 +0300

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import * as cookieParser from 'cookie-parser';
 import { SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
-import { loggerConfig, swaggerConfig } from './config';
+import { loggerConfig, swaggerConfig } from './common/configs';
+import { configureCors } from './common/configs/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, loggerConfig);
 
   app.use(cookieParser());
-  app.enableCors();
+  configureCors(app);
 
   const documentFactory = () =>
     SwaggerModule.createDocument(app, swaggerConfig);
