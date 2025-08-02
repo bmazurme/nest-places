8a8d51674b518fad41d66458b9b0f0b807987134
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-02 19:15:06 +0300

    fix: users.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,10 +8,10 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { User } from './entities/user.entity';
+import { Role } from '../roles/entities/role.entity';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Role } from '../roles/entities/role.entity';
 
 @Injectable()
 export class UsersService {
