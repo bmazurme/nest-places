4a264f1d12a5e72305465762588dacf1be2586b2
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-03 22:59:33 +0300

    fix: cards.service

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,10 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Card } from './entities/card.entity';
 import { User } from '../users/entities/user.entity';
 
+// import { CardDto } from './dto/card.dto';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
 
@@ -44,5 +49,35 @@ export class CardsService {
 
   remove(id: number) {
     return this.cardRepository.delete(id);
+  }
+
+  async getCount(id: number) {
+    const count = await this.cardRepository.countBy({
+      user: { id },
+    });
+
+    return { count };
+  }
+
+  async getCardsByUser(userId: number, page: number, user: User) {
+    if (Number.isNaN(userId) || Number.isNaN(page)) {
+      return new BadRequestException();
+    }
+
+    return this.cardRepository.query(
+      `
+        SELECT t.id, t.name, t.link, t."userId" "userId", t.count::int, t."isLiked", u.name userName
+        FROM (SELECT c.id, c.name, c.link, c."userId", COUNT(l."cardId") as count, bool_or(l."userId" = $2) as "isLiked"
+            FROM card c
+            LEFT JOIN "like" l ON c.id = l."cardId"
+            WHERE c."userId" = $1
+            GROUP BY c.id) t
+        LEFT JOIN "user" u ON t."userId" = "u".id
+        ORDER BY t.id DESC
+        OFFSET $3 ROWS
+        FETCH NEXT 3 ROWS ONLY
+      `,
+      [userId, user.id, page - 1],
+    );
   }
 }
