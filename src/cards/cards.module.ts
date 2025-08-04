f7003e803721aa00fe8188433a4f0001c8b83687
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-04 21:57:43 +0300

    fix: cards.module

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { CardsService } from './cards.service';
 
@@ -8,7 +10,18 @@ import { Card } from './entities/card.entity';
 import { LikesModule } from '../likes/likes.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Card]), LikesModule],
+  imports: [
+    TypeOrmModule.forFeature([Card]),
+    LikesModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule.forRoot()],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET') ?? 'SECRET',
+        signOptions: { expiresIn: '10d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [CardsController],
   providers: [CardsService],
   exports: [CardsService],
