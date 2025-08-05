0747095882419071cf3b6229f7aec12253894eb3
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-05 21:43:55 +0300

    fix: cards.module

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -8,11 +8,13 @@ import { CardsService } from './cards.service';
 import { CardsController } from './cards.controller';
 import { Card } from './entities/card.entity';
 import { LikesModule } from '../likes/likes.module';
+import { TagsModule } from '../tags/tags.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Card]),
     LikesModule,
+    TagsModule,
     JwtModule.registerAsync({
       imports: [ConfigModule.forRoot()],
       useFactory: async (configService: ConfigService) => ({
