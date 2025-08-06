1f83809659c5f950c655566b891b9c8cf4a41abe
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-08-06 17:12:20 +0300

    fix: cards.service

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,15 +1,18 @@
 import {
   BadRequestException,
   Injectable,
-  NotFoundException,
+  // NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Card } from './entities/card.entity';
 import { User } from '../users/entities/user.entity';
+// import { Tag } from '../tags/entities/tag.entity';
 
-// import { CardDto } from './dto/card.dto';
+import { LikesService } from '../likes/likes.service';
+import { TagsService } from 'src/tags/tags.service';
+
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
 
@@ -18,29 +21,29 @@ export class CardsService {
   constructor(
     @InjectRepository(Card)
     private readonly cardRepository: Repository<Card>,
+    private readonly likesService: LikesService,
+    private readonly tagsService: TagsService,
   ) {}
 
-  create(createCardDto: CreateCardDto, user: User) {
-    return this.cardRepository.save({ ...createCardDto, user });
+  async create(createCardDto: CreateCardDto, user: User) {
+    const tag = await this.tagsService.findByNameOrCreate(
+      createCardDto.tagName,
+    );
+
+    const card = new Card();
+
+    card.name = createCardDto.name;
+    card.link = createCardDto.link;
+    card.user = user;
+    card.tags = [tag];
+
+    return this.cardRepository.save(card);
   }
 
   async findAll() {
     return await this.cardRepository.find({
       relations: ['tags'],
     });
-  }
-
-  async findOne(id: number) {
-    const card = await this.cardRepository.find({
-      where: { id },
-      relations: ['tags'],
-    });
-
-    if (!card) {
-      throw new NotFoundException(`card with id ${id} not found`);
-    }
-
-    return card;
   }
 
   update(id: number, updateCardDto: UpdateCardDto) {
@@ -77,7 +80,123 @@ export class CardsService {
         OFFSET $3 ROWS
         FETCH NEXT 3 ROWS ONLY
       `,
-      [userId, user.id, page - 1],
+      [userId, user.id, (page - 1) * 3],
     );
+  }
+
+  async getCardsByTag(tagName: string, page: number, user: User) {
+    if (Number.isNaN(page)) {
+      return new BadRequestException();
+    }
+
+    return this.cardRepository.query(
+      `
+        SELECT rslt.id, rslt.name, rslt.link, rslt."userId" userid, rslt.count::int, rslt.liked isliked, rslt.userName, rslt.tagsname tagsname
+        FROM (SELECT tt.id, tt.name, tt.link, tt."userId", tt.count::int, tt.liked, tt.userName, tt.tagid, tag.name tagsname
+            FROM (SELECT t.id, t.name, t.link, t."userId", t.count::int, t.liked, u.name userName, tg."tagId" tagid
+                FROM (SELECT c.id, c.name, c.link, c."userId", COUNT(l."cardId") as count, bool_or(l."userId" = $2) as liked
+                    FROM card c
+                    LEFT JOIN "like" l ON c.id = l."cardId"
+                    GROUP BY c.id) t
+                LEFT JOIN "user" u ON t."userId" = u.id
+                LEFT JOIN "cardTags" tg ON t.id = tg."cardId") tt
+            LEFT JOIN "tag" ON tt.tagid = tag.id) rslt
+        WHERE tagsname = $1
+        ORDER BY rslt.id DESC
+        OFFSET $3 ROWS
+        FETCH NEXT 3 ROWS ONLY
+      `,
+      [tagName, user.id, (page - 1) * 3],
+    );
+  }
+
+  async getCardsByPage(page: number, currentUser: number) {
+    if (Number.isNaN(page)) {
+      return new BadRequestException();
+    }
+
+    return this.cardRepository.query(
+      `
+        SELECT t.id, t.name, t.link, t."userId" "userId", t.count::int, t.isLiked, u.name userName
+        FROM (SELECT c.id, c.name, c.link, c."userId", COUNT(l."cardId") as count, bool_or(l."userId" = $1) as isLiked
+            FROM card c
+            LEFT JOIN "like" l ON c.id = l."cardId"
+            GROUP BY c.id) t
+        LEFT JOIN "user" u ON t."userId" = "u".id
+        ORDER BY t.id DESC
+        OFFSET $2 ROWS
+        FETCH NEXT 3 ROWS ONLY
+      `,
+      [currentUser, (page - 1) * 3],
+    );
+  }
+
+  async getCardById(cardId: number, userId: number) {
+    if (Number.isNaN(cardId)) {
+      return new BadRequestException();
+    }
+
+    const [card] = await this.cardRepository.query(
+      `
+        SELECT t.id, t.name, t.link, t."userId" userId, t.count::int, t."isLiked", u.name "userName"
+        FROM (SELECT c.id, c.name, c.link, c."userId", COUNT(l."cardId") as count, bool_or(l."userId" = $2) as "isLiked"
+            FROM card c
+            LEFT JOIN "like" l ON c.id = l."cardId"
+            WHERE c.id = $1
+            GROUP BY c.id) t
+        LEFT JOIN "user" u ON t."userId" = "u".id
+        ORDER BY t.id DESC
+      `,
+      [cardId, userId],
+    );
+    return card;
+  }
+
+  async likeCard({ id }: { id: number }, user: User) {
+    if (Number.isNaN(id)) {
+      return new BadRequestException();
+    }
+
+    await this.likesService.like({ user, card: { id } });
+
+    const [card] = await this.cardRepository.query(
+      `
+        SELECT t.id, t.name, t.link, t."userId" userId, t.count::int, t.isLiked, u.name userName
+        FROM (SELECT c.id, c.name, c.link, c."userId", COUNT(l."cardId") as count, bool_or(l."userId" = $2) as isLiked
+            FROM card c
+            LEFT JOIN "like" l ON c.id = l."cardId"
+            WHERE c.id = $1
+            GROUP BY c.id) t
+        LEFT JOIN "user" u ON t."userId" = u.id
+        ORDER BY t.id DESC
+      `,
+      [id, user.id],
+    );
+
+    return card;
+  }
+
+  async dislikeCard({ id }: { id: number }, user: User) {
+    if (Number.isNaN(id)) {
+      return new BadRequestException();
+    }
+
+    await this.likesService.dislike({ user, card: { id } });
+
+    const [card] = await this.cardRepository.query(
+      `
+        SELECT t.id, t.name, t.link, t."userId" userId, t.count::int, t.isLiked, u.name userName
+        FROM (SELECT c.id, c.name, c.link, c."userId", COUNT(l."cardId") as count, bool_or(l."userId" = $2) as isLiked
+            FROM card c
+            LEFT JOIN "like" l ON c.id = l."cardId"
+            WHERE c.id = $1
+            GROUP BY c.id) t
+        LEFT JOIN "user" u ON t."userId" = u.id
+        ORDER BY t.id DESC
+      `,
+      [id, user.id],
+    );
+
+    return card;
   }
 }
