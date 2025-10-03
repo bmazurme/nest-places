4d82e181ac654f81060e0b3a3d73b0df351a45c0
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-03 22:16:15 +0300

    fix: users.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,8 +13,6 @@ import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-// const TARGET_URL = 'http://localhost:3005';
-
 @Injectable()
 export class UsersService {
   constructor(
