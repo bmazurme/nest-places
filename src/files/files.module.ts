b3434b7ae83c8efc685e127999355ece9ae0290f
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-03 22:24:43 +0300

    fix: files.module

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
-// import { existsSync, mkdirSync } from 'fs';
-// import { join } from 'path';
-
-// import { User } from '../users/entities/user.entity';
 
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
@@ -13,23 +9,6 @@ import { UsersModule } from '../users/users.module';
 @Module({
   imports: [
     UsersModule,
-    // MulterModule.register({
-    //   storage: diskStorage({
-    //     destination: './uploads',
-    //     filename: async (req, file, cb) => {
-    //       const userFolder = (req.user as User).id.toString();
-    //       const targetPath = join(__dirname, '..', '..', 'uploads', userFolder);
-
-    //       if (!existsSync(targetPath)) {
-    //         mkdirSync(targetPath);
-    //       }
-
-    //       const path = `${userFolder}/${Date.now()}-${file.originalname}`;
-
-    //       cb(null, path);
-    //     },
-    //   }),
-    // }),
     MulterModule.register({
       storage: memoryStorage(), // Используем memoryStorage вместо diskStorage
       limits: { fileSize: 10 * 1024 * 1024 }, // ограничение в 10MB
