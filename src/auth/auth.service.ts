f9e3b07b9fb8503653851f85633216cf85b23600
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-06 22:10:01 +0300

    fix: auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+import { User } from '../users/entities/user.entity';
+import { TARGET_URL } from '../common/configs/constants';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+  ) {}
+
   async logout(response: Response) {
     response.clearCookie('access_token', {
       httpOnly: true,
@@ -11,5 +19,21 @@ export class AuthService {
       sameSite: 'strict',
       maxAge: 0,
     });
+  }
+
+  async signin(currentUser: User, response: Response) {
+    const payload = {
+      sub: currentUser.id,
+      roles: (currentUser as User & { roles: string[] }).roles.map(
+        ({ name }) => name,
+      ),
+    };
+
+    response.cookie('access_token', this.jwtService.sign(payload), {
+      httpOnly: true,
+      maxAge: 3600000,
+    });
+
+    response.redirect(TARGET_URL);
   }
 }
