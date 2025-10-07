dc1162bfd29fe16458c10caef0badc621bc512cf
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-07 20:17:52 +0300

    fix: app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { OAuthModule } from './oauth/oauth.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TagsModule } from './tags/tags.module';
 import { CardsModule } from './cards/cards.module';
@@ -19,13 +20,14 @@ import { TypeOrmModuleConfig } from './common/configs';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
-      serveStaticOptions: { index: false },
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'uploads'),
+    //   serveStaticOptions: { index: false },
+    // }),
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModuleConfig,
     OAuthModule,
+    AuthModule,
     UsersModule,
     TagsModule,
     CardsModule,
