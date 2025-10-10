e9b359f6f1036d919c229a37dad0446ebad4b084
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-10 19:05:48 +0300

    fix: auth.controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,20 +5,32 @@ import {
   ClassSerializerInterceptor,
   UseGuards,
   Res,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-import { RolesGuard } from '../common/guards/roles.guard';
 import { JwtGuard } from '../common/guards/jwt.guard';
 import { AuthService } from './auth.service';
 
-@UseGuards(JwtGuard, RolesGuard)
+@UseGuards(JwtGuard)
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/logout')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'Logout user' })
+    @ApiResponse({
+    status: 200,
+    description: 'User successfully logged out',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found',
+  })
   logout(@Res({ passthrough: true }) response: Response) {
     return this.authService.logout(response);
   }
