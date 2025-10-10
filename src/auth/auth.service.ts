4bd723c711f2bddbd49ccbba6d498cf5131b4f8e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-10 19:08:08 +0300

    fix: auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { Response } from 'express';
 import { JwtService } from '@nestjs/jwt';
 
@@ -7,33 +7,47 @@ import { TARGET_URL } from '../common/configs/constants';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger('AuthService');
+
   constructor(
     private readonly jwtService: JwtService,
   ) {}
 
   async logout(response: Response) {
-    response.clearCookie('access_token', {
-      httpOnly: true,
-      secure: true,
-      // secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 0,
-    });
+    try {
+      response.clearCookie('access_token', {
+        httpOnly: true,
+        secure: true,
+        // secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 0,
+      });
+      this.logger.debug('User successfully logged out');
+    } catch (error) {
+      this.logger.error('Failed to logout', error);
+      throw new HttpException('Failed to logout', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   async signin(currentUser: User, response: Response) {
-    const payload = {
-      sub: currentUser.id,
-      roles: (currentUser as User & { roles: string[] }).roles.map(
-        ({ name }) => name,
-      ),
-    };
+    try {
+      const payload = {
+        sub: currentUser.id,
+        roles: (currentUser as User & { roles: string[] }).roles.map(
+          ({ name }) => name,
+        ),
+      };
 
-    response.cookie('access_token', this.jwtService.sign(payload), {
-      httpOnly: true,
-      maxAge: 3600000,
-    });
+      response.cookie('access_token', this.jwtService.sign(payload), {
+        httpOnly: true,
+        maxAge: 3600000,
+      });
 
-    response.redirect(TARGET_URL);
+      response.redirect(TARGET_URL);
+      this.logger.debug('User successfully signed in');
+    } catch (error) {
+      this.logger.error('Failed to sign in', error);
+      throw new HttpException('Failed to sign in', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
