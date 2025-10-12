b0241483f3174a6c07fc28796df49fae51a4daa7
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-12 16:54:43 +0300

    fix: main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import helmet from 'helmet';
 
 import { SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { loggerConfig, swaggerConfig } from './common/configs';
 import { configureCors } from './common/configs/cors.config';
+import { helmetConfig } from './common/configs/helmet-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, loggerConfig);
 
   app.use(cookieParser());
+  app.use(compression());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.contentSecurityPolicy(helmetConfig));
+
   configureCors(app);
 
   const documentFactory = () =>
