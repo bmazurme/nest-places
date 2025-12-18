d765577f4101ca4dd6e1f5922487e9566f33de81
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-12-18 20:36:15 +0300

    fix: app.controller.spec

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,23 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let app: TestingModule;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    app = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          // Токен, который NestJS ищет для метрики 'get_hello_calls'
+          provide: 'PROM_METRIC_GET_HELLO_CALLS',
+          useValue: {
+            // Мокируем метод inc() — достаточно пустой функции для теста
+            inc: jest.fn(),
+          },
+        },
+      ],
     }).compile();
-
-    appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      const controller = app.get(AppController);
+      const result = controller.getHello();
+      expect(result).toBe('Hello World!');
     });
   });
 });
