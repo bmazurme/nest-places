283c68d5a616aa94a18d85233f29395acd284649
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-02-25 20:52:56 +0300

    fix: auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,6 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
-
 @Module({
   imports: [
     JwtModule.registerAsync({
